8070cfec19ee59d9718707f9374eb7f276d076ea
ibuler <ibuler@qq.com>
2016-05-06 17:40:26 +0800

    完成paramiko交互式连接服务器
    需要注意:
    1. 写TTY类时,self.ssh存在是必要的,否则改变量如果不赋给对象,会被python回
    收掉
    2. posix_shell是实现的关键使用select来监控文件描述符
    3. tty模块使用

--- connect.py
+++ connect.py
@@ -32,3 +32,87 @@ except ImportError:
     time.sleep(3)
     sys.exit()
 
+
+username = 'root'
+password = 'redhat'
+host = '192.168.244.129'
+port = 22
+
+
+def color_print(msg, color='red', exit=False):
+    """
+    Print colorful string.
+    颜色打印字符或者退出
+    """
+    color_msg = {'blue': '\033[1;36m{0}\033[0m',
+                 'green': '\033[1;32m{0}\033[0m',
+                 'yellow': '\033[1;33m{0}\033[0m',
+                 'red': '\033[1;31m{0}\033[0m',
+                 'title': '\033[30;42m{0}\033[0m',
+                 'info': '\033[32m{0}\033[0m'}
+    msg = color_msg.get(color, 'red').format(msg)
+    print(msg)
+    if exit:
+        time.sleep(2)
+        sys.exit()
+    return msg
+
+
+class TTY:
+    def __init__(self, host='127.0.0.1', port=22, username='root', password=''):
+        self.host = host
+        self.port = port
+        self.username = username
+        self.password = password
+        self.ssh = None
+        self.chan = None
+        self.__get_chan()
+
+    def __get_chan(self):
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            ssh.connect(self.host,
+                        port=self.port,
+                        username=self.username,
+                        password=self.password)
+        except (paramiko.ssh_exception.AuthenticationException, paramiko.ssh_exception.SSHException):
+            color_print('连接服务器失败', exit=True)
+        self.ssh = ssh
+        self.chan = ssh.invoke_shell(term='xterm')
+
+    def posix_shell(self):
+        old_tty = termios.tcgetattr(sys.stdin)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            tty.setcbreak(sys.stdin.fileno())
+            self.chan.settimeout(0.0)
+
+            while True:
+                try:
+                    r, w, e = select.select([self.chan, sys.stdin], [], [])
+                except:
+                    pass
+
+                if self.chan in r:
+                    try:
+                        recv_data = self.chan.recv(1024).decode('utf8')
+                        if len(recv_data) == 0:
+                            break
+                        sys.stdout.write(recv_data)
+                        sys.stdout.flush()
+                    except socket.timeout:
+                        print('Timeout')
+                if sys.stdin in r:
+                    x = os.read(sys.stdin.fileno(), 1024)
+                    if len(x) == 0:
+                        break
+                    self.chan.send(x)
+
+        finally:
+            termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_tty)
+
+
+if __name__ == '__main__':
+    tty_ = TTY(host=host, port=port, username=username, password=password)
+    tty_.posix_shell()
