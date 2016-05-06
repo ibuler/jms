ea8e81a06b1e4f784fb64153ddb47a7b1eaa54f4
ibuler <ibuler@qq.com>
2016-05-06 23:54:05 +0800

    基本完成用户的添加
    瑕疵:
    用户添加不仅仅是在数据库中, 需要用户通过ssh登录,一种方法是实现ssh server
    另一种简单的方法就是利用原生的sshd,我们在服务器上建立用户

--- user/forms.py
+++ user/forms.py
@@ -3,3 +3,17 @@
 # Created by guang on 
 # 
 
+from django.contrib.auth.models import User
+from django.forms import ModelForm, TextInput, EmailInput, PasswordInput
+
+
+class UserAddForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'password', 'email']
+        widgets = {
+            'username': TextInput(attrs={'placeholder': 'username'}),
+            'password': PasswordInput(attrs={'placeholder': 'password'}),
+            'email': EmailInput(attrs={'placeholder': 'email'})
+
+        }
--- user/views.py
+++ user/views.py
@@ -1,15 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 
 from django.contrib.auth.models import User
+
+from .forms import UserAddForm
 
 # Create your views here.
 
 
 def user_add(request):
-    return render(request, 'base.html')
+    form = UserAddForm(request.POST)
+    if form.is_valid():
+        password = form.cleaned_data['password']
+        user = form.save(commit=False)
+        user.set_password(password)
+        user.save()
+        return HttpResponseRedirect(reverse('user:list'))
+    else:
+        return HttpResponse('验证失败')
 
 
 def user_list(request):
     users = User.objects.all()
-    return render(request, 'user/list.html', {'users': users})
+    form = UserAddForm()
+    return render(request, 'user/list.html', {'users': users, 'form': form})
