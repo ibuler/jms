5bc55ccc818664077b0cd0a8c4258cd23f7f47b6
ibuler <ibuler@qq.com>
2016-05-06 23:12:20 +0800

    完成基础模板 base.html的制作
    使用添加用户view进行模板测试
    
    模板来源: http://semantic-ui.com/usage/layout.html

--- user/urls.py
+++ user/urls.py
@@ -3,3 +3,13 @@
 # Created by guang on 
 # 
 
+
+from django.conf.urls import url
+
+from . import views
+
+
+app_name = 'user'
+urlpatterns = [
+    url(r'^add/$', views.user_add, name='add'),
+]
--- user/views.py
+++ user/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def user_add(request):
+    return render(request, 'base.html')
