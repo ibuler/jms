8001855d9a56cc82170aace5957d57cd9536209c
ibuler <ibuler@qq.com>
2016-05-07 16:58:33 +0800

    完成 asset的数据模型
    注册到Admin,可以通过Admin的管理

--- asset/admin.py
+++ asset/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Asset
+
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    pass
--- asset/models.py
+++ asset/models.py
@@ -1,3 +1,24 @@
+# coding: utf-8
+
 from django.db import models
 
 # Create your models here.
+
+
+class Asset(models.Model):
+    OS_CHOICE = [
+        ('C', 'CentOS'),
+        ('U', 'Ubuntu'),
+        ('D', 'Debian'),
+        ('R', 'Redhat'),
+        ('B', 'BSD'),
+    ]
+    ip = models.GenericIPAddressField(verbose_name='IP', unique=True)
+    port = models.IntegerField(default=22, verbose_name='端口')
+    username = models.CharField(max_length=20, verbose_name='管理用户名')
+    password = models.CharField(max_length=32, verbose_name='管理密码')
+    os = models.CharField(choices=OS_CHOICE, max_length=2, verbose_name='系统平台')
+    is_active = models.BooleanField(default=True, verbose_name='激活')
+
+    def __str__(self):
+        return self.ip
